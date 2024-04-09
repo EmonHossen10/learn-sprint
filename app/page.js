dbbcc760f770b2cacb157e0c1819ae9c7a0944bf
unknown <emonhossen1000@gmail.com>
2024-04-09 14:52:06 +0600

    testing purpose

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
+ 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main >
      <h2>hello world</h2>
-     <h2>Development branch made</h2>
+     <h1>This is Emon </h1>
     </main>
   );
 }
