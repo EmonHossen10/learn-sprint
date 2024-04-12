d339b58fd378ed00b1feacef0975cf60c0b4035c
unknown <emonhossen1000@gmail.com>
2024-04-13 00:24:54 +0600

    not changed

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
- 
-
 export default function Home() {
   return (
     <main>
-      <h2 className="text-4xl text-center py-10 text-sky-500 ">This is Home page</h2>
-      <h2 className="text-4xl text-center  text-sky-500 ">Here another content is commingggggg......</h2>
-      
+      <h2 className="text-4xl text-center py-10 text-sky-500 ">
+        This is Home page
+      </h2>
+      <h2 className="text-4xl text-center  text-sky-500 ">
+        Here another content is commingggggg......
+      </h2>
     </main>
   );
 }
