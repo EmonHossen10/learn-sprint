64bb5e885a18445ac98b1b817207a4fcb797db14
unknown <you@example.com>
2024-04-13 15:43:40 +0600

    added again updates

--- src/components/ui/Banner/Banner.js
+++ src/components/ui/Banner/Banner.js
@@ -8,37 +8,15 @@ import "./nextBanner.css";
 const Banner = () => {
   return (
     <div className="nextBanner bg-opacity-10">
-      {/* <div className="flex">
+      <div className="flex">
         <div className="flex">
           <Image src={bannerImg} width={600} alt="banner" />
         </div>
         <div style={{ width: "30%" }}>
           <Lottie animationData={lottieProgrammar} />
         </div>
-      </div> */}
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: "url('../../../../public/assets/banner221.png')",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-            <div style={{ width: "30%" }}>
-              <Lottie animationData={lottieProgrammar} />
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
+     </div>
   );
 };
 
