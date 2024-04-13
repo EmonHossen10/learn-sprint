fef2327eeee9ca3d16a2585bccb042344f98a3ab
unknown <you@example.com>
2024-04-13 11:22:19 +0600

    added updates

--- src/components/ui/Banner/Banner.js
+++ src/components/ui/Banner/Banner.js
@@ -1,26 +1,23 @@
-"use client"
-import bannerImg from "../../../../public/assets/banner.png"
+"use client";
+import bannerImg from "../../../../public/assets/banner.png";
 import Image from "next/image";
-import lottieProgrammar from "../../../../public/programmer.json"
-import Lottie from "lottie-react"
-import "./nextBanner.css"
-
+import lottieProgrammar from "../../../../public/programmer.json";
+import Lottie from "lottie-react";
+import "./nextBanner.css";
 
 const Banner = () => {
-    return (
-        <div className="nextBanner h-screen md:flex justify-center items-center">
-            <div>
-                <Image 
-                    src={bannerImg}
-                    width={500}
-                    alt="banner"
-                />
-            </div>
-            <div style={{width:"50%"}}>
-                <Lottie animationData={lottieProgrammar} />
-            </div>
+  return (
+    <div className="nextBanner">
+      <div className="md:flex justify-center items-center">
+        <div>
+          <Image src={bannerImg} width={500} alt="banner" />
         </div>
-    );
+        <div style={{ width: "50%" }}>
+          <Lottie animationData={lottieProgrammar} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
