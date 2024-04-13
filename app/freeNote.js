6b5735bd10c12457a5992091ad7ef7236689eb65
Amit Kumar <aka883207@gmail.com>
2024-04-13 22:16:01 +0600

    freenotes file updatedgit add .!

--- app/freeNote.js
+++ app/freeNote.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { IoCloudDownloadOutline } from "react-icons/io5";
 export default function FreeNotes() {
     return (
-        <secttion className="max-w-7xl mx-auto bg-black grid grid-cols-4 rounded-lg mt-10">
+        <secttion className="max-w-7xl mx-auto bg-black grid grid-cols-4 rounded-lg mt-10 mb-10">
             <div className='grid items-center col-span-3'>
                 <div className='ml-20'>
                     <h1 className='font-semibold text-4xl mb-5'>Get Exclusive <span className='text-[#1CAB55]'>notes!!!</span> created by our <br /> Experienced Teachers.</h1>
